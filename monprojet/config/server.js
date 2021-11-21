bbb7642bdc60ddd5602400103bccbafeb55e5bc6
Freelanceweb16 <marketing@freelanceweb16.fr>
2021-11-21 19:10:49 +0100

    Projet Strapi - API Rest/GraphQL

---
+++ monprojet/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c8265953a75b758be951a712878b4be9'),
+    },
+  },
+});
---
+++ monprojet/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fc8f8b36-aaff-45d4-bdd1-7fe5bc0bd18e'
+};
